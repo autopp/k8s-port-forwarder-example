fde84eb23e666c07632d8f011a293b5e52f23fd8
autopp <autopp.inc@gmail.com>
2020-08-23 01:17:23 +0900

    feat: impl skelton

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/autopp/k8s-port-forwarder-example
 go 1.14
 
 require (
-	k8s.io/api v0.18.8 // indirect
-	k8s.io/client-go v11.0.0+incompatible
+	github.com/evanphx/json-patch v4.2.0+incompatible // indirect
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d // indirect
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
+	k8s.io/cli-runtime v0.18.8
+	k8s.io/client-go v0.18.8
+	k8s.io/utils v0.0.0-20200821003339-5e75c0163111 // indirect
 )
--- main.go
+++ main.go
@@ -1,11 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"os"
 
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/portforward"
+	"k8s.io/client-go/transport/spdy"
 )
 
+type MyPortForwardOptions struct {
+	configFlags *genericclioptions.ConfigFlags
+}
+
 func main() {
-	fmt.Println(portforward.ForwardedPort{})
+	transport, upgrader, err := spdy.RoundTripperFor(&rest.Config{})
+	if err != nil {
+		panic(err)
+	}
+	dialer := spdy.NewDialer(upgrader, &http.Client{Transport: transport}, "POST", nil)
+	fw, err := portforward.New(dialer, nil, nil, nil, os.Stdout, os.Stderr)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fw.ForwardPorts()
 }
